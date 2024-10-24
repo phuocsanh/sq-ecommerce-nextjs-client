b3f6aaee5980dde63787ce3155692da15b1a1215
phuocsanh <phuocsanhtps@gmail.com>
2024-10-24 23:00:55 +0700

    udpate

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,12 +1,45 @@
 import authApiRequest from "@/apiRequest/auth";
 import { LoginBodyType } from "@/schemaValidations/auth.schema";
 import { cookies } from "next/headers";
-
+import jwt from "jsonwebtoken";
+import { HttpError } from "@/lib/http";
+import { error } from "console";
 export async function POST(request: Request) {
   const body = (await request.json()) as LoginBodyType;
   const cookieStore = cookies();
   try {
     const { payload } = await authApiRequest.sLogin(body);
+    const { accessToken, refreshToken } = payload.data;
+    const decodeAccessToken = jwt.decode(accessToken) as { exp: number };
+    const decodeRefreshToken = jwt.decode(refreshToken) as { exp: number };
+    cookieStore.set("accessToken", accessToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      expires: decodeAccessToken.exp * 1000,
+    });
+    cookieStore.set("refreshToken", refreshToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      expires: decodeRefreshToken.exp * 1000,
+    });
+    return Response.json(payload);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (e) {}
+  } catch (e) {
+    if (e instanceof HttpError) {
+      return Response.json(e.payload, {
+        status: e.status,
+      });
+    } else {
+      return Response.json(
+        {
+          message: "Có lỗi xảy ra",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
 }
--- src/app/components/BtnLogin.tsx
+++ src/app/components/BtnLogin.tsx
@@ -1,17 +1,10 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import React from "react";
-import { useLoginMutation } from "@/tanstack-queries/use-auth";
 
 function BtnLogin() {
-  const loginUser = useLoginMutation();
   return (
-    <Button
-      onClick={() => {
-        loginUser.mutateAsync();
-      }}
-      title="Click"
-    >
+    <Button onClick={() => {}} title="Click">
       Click
     </Button>
   );
